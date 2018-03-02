3e6ca292c9e9aefcfaa3e8e9bd60141254f8945f
RozanskiT <tomasz.rozanski95@gmail.com>
2018-03-02 16:22:10 +0100

    initial

---
+++ LCG_predict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Assignment 1.
+
+Construct and implement an efficient statistical test that predicts (with
+non-negligible probability) next bits of linear congruencial generator.
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+	main()
